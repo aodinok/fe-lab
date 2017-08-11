023b7ccd8d35ae49497fed2c40eb02886b3e9bd8
Aleksandr Odinok <aodinok@lohika.com>
2017-08-12 00:48:11 +0300

    Build web UI project using Dockerfile before starting machine

--- web/src/App.js
+++ web/src/App.js
@@ -4,8 +4,14 @@ import styles from './App.css'
 
 const App = () => (
   <grid className='grid'>
-    <header className='header'>header content</header>
-    <content className='content'>blog site content</content>
+    <header className='header'><button onClick={() => {
+      fetch('/api/1.0/users').then((data) => {
+        data.json().then((users) => {
+          document.getElementById('users').innerHTML = JSON.stringify(users)
+        })
+      })
+    }}></button></header>
+    <content className='content'>blog site content<div id='users' /></content>
     <footer className='footer'>Odessa, 2017</footer>
   </grid>
 )
